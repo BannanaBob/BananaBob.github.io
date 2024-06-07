5f5f2758ae0fca7f61fbf1dec33fe9caac70fe50
BannanaBob <144619962+BannanaBob@users.noreply.github.com>
2024-06-07 22:22:22 +0200

    Update script_2.js

--- js/script_2.js
+++ js/script_2.js
@@ -20,7 +20,7 @@ menu.onclick = () =>{
           setTimeout(function() {
             // Directly navigate to the next page
             window.location.href = "index_EN.html";
-          }, 1500); // Adjust the delay to match your animation duration
+          }, 2300); // Adjust the delay to match your animation duration
         });
       });
 
@@ -34,7 +34,7 @@ menu.onclick = () =>{
     setTimeout(function() {
       // Directly navigate to the next page
       window.location.href = "index.html";
-    }, 1500); // Adjust the delay to match your animation duration
+    }, 2300); // Adjust the delay to match your animation duration
   });
 });
 
